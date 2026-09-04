8b95d5a6b867277c2e923d3a03cb6d231833a417
Matthew Hon <matthewhon@honfamily.com>
2026-09-04 12:30:09 -0500

    fiex

--- app/next.config.ts
+++ app/next.config.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  outputFileTracingRoot: path.join(__dirname, '../'),
+  outputFileTracingRoot: path.join(__dirname),
   experimental: {
     serverActions: {
       allowedOrigins: ['localhost:3000'],
